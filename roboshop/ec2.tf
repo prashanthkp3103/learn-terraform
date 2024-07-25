ccd57970e83140bb5a18337cb4477dc7a9b5168d
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-07-25 14:07:51 +0530

    terraform

--- roboshop/ec2.tf
+++ roboshop/ec2.tf
@@ -1,8 +1,31 @@
 resource "aws_instance" "web" {
   ami           = "ami-041e2ea9402c46c32"
+  vpc_security_group_ids = ["sg-03ec8b1ac2604e9da"]
   instance_type = "t3.small"
 
   tags = {
     Name = "frontend"
+  }
+}
+
+
+resource "aws_instance" "mongo" {
+  ami           = "ami-041e2ea9402c46c32"
+  vpc_security_group_ids = ["sg-03ec8b1ac2604e9da"]
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "mongo"
+  }
+}
+
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-041e2ea9402c46c32"
+  vpc_security_group_ids = ["sg-03ec8b1ac2604e9da"]
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "catalogue"
   }
 }
