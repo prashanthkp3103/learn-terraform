9c87ea05312f0c771f18c7636462de809a985ce8
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-07-26 15:21:48 +0530

    terraform

--- roboshop-v3/ec2.tf
+++ roboshop-v3/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "instance" {
   count = length(var.components)
   ami           = data.aws_ami.ami.image_id
   vpc_security_group_ids = data.aws_security_groups.sg.ids
@@ -8,15 +8,15 @@ resource "aws_instance" "frontend" {
     Name = "${var.components[count.index]}dev"
   }
 }
-# resource "aws_route53_record" "frontend" {
-#   zone_id = data.aws_route53_zone.zone.zone_id
-#   name    = "frontend.dev.${var.domain_name}"
-#   type    = "A"
-#   ttl     = 15
-#   records = [aws_instance.frontend.private_ip]
-# }
-#
-#
+resource "aws_route53_record" "frontend" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "frontend.dev.${var.domain_name}"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.instance.private_ip[count.index]]
+}
+
+
 # resource "aws_instance" "mongo" {
 #   count = length(var.components)
 #   ami           = data.aws_ami.ami.image_id
