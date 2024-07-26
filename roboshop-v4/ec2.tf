c5889ab483ccce0c983f362a86293945491fbc38
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-07-26 15:45:19 +0530

    terraform

--- roboshop-v4/ec2.tf
+++ roboshop-v4/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   for_each = var.components
   ami           = data.aws_ami.ami.image_id
   vpc_security_group_ids = data.aws_security_groups.sg.ids
-  instance_type = each.key["instance_type"] # as instance type is a map inside component variable
+  instance_type = each.value["instance_type"] # as instance type is a map inside component variable
 
   tags = {
     Name = "${each.key}dev"
