db4babca7cce4665ca01292d94c8ddcfb3c185ab
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-07-26 13:27:05 +0530

    terraform

--- roboshop/ec2.tf
+++ roboshop/ec2.tf
@@ -1,16 +1,34 @@
+variable "ami" {
+  default = "ami-041e2ea9402c46c32"
+}
+
+variable "instance_type" {
+  default = "t3.small"
+}
+
+variable "vpc_security_group_ids" {
+  default = "sg-03ec8b1ac2604e9da"
+}
+
+variable "zoneid" {
+  default = "Z0668859214N41P8Y7GLH"
+}
+
+variable "domain_name" {
+  default = "meppk.xyz"
+}
 resource "aws_instance" "frontend" {
-  ami           = "ami-041e2ea9402c46c32"
-  vpc_security_group_ids = ["sg-03ec8b1ac2604e9da"]
-  instance_type = "t3.small"
+  ami           = var.ami
+  vpc_security_group_ids = [var.vpc_security_group_ids]
+  instance_type = var.instance_type
 
   tags = {
     Name = "frontend.dev"
   }
 }
-
 resource "aws_route53_record" "frontend" {
-  zone_id = "Z0668859214N41P8Y7GLH"
-  name    = "meppk.xyz"
+  zone_id = var.zoneid
+  name    = "frontend.dev.${var.domain_name}"
   type    = "A"
   ttl     = 15
   records = [aws_instance.frontend.private_ip]
@@ -18,9 +36,9 @@ resource "aws_route53_record" "frontend" {
 
 
 resource "aws_instance" "mongo" {
-  ami           = "ami-041e2ea9402c46c32"
-  vpc_security_group_ids = ["sg-03ec8b1ac2604e9da"]
-  instance_type = "t3.small"
+  ami           = var.ami
+  vpc_security_group_ids = var.vpc_security_group_ids
+  instance_type = var.instance_type
 
   tags = {
     Name = "mongo.dev"
@@ -28,8 +46,8 @@ resource "aws_instance" "mongo" {
 }
 
 resource "aws_route53_record" "mongo" {
-  zone_id = "Z0668859214N41P8Y7GLH"
-  name    = "meppk.xyz"
+  zone_id = var.zoneid
+  name    = "mongo.dev.${var.domain_name}"
   type    = "A"
   ttl     = 15
   records = [aws_instance.mongo.private_ip]
@@ -37,9 +55,9 @@ resource "aws_route53_record" "mongo" {
 
 
 resource "aws_instance" "catalogue" {
-  ami           = "ami-041e2ea9402c46c32"
-  vpc_security_group_ids = ["sg-03ec8b1ac2604e9da"]
-  instance_type = "t3.small"
+  ami           = var.ami
+  vpc_security_group_ids = var.vpc_security_group_ids
+  instance_type = var.instance_type
 
   tags = {
     Name = "catalogue.dev"
@@ -47,8 +65,8 @@ resource "aws_instance" "catalogue" {
 }
 
 resource "aws_route53_record" "catalogue" {
-  zone_id = "Z0668859214N41P8Y7GLH"
-  name    = "meppk.xyz"
+  zone_id = var.zoneid
+  name    = "catalogue.dev.${var.domain_name}"
   type    = "A"
   ttl     = 15
   records = [aws_instance.catalogue.private_ip]
