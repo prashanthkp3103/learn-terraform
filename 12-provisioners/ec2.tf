70ad12c26c21377128c3d95f8816b87a4100f868
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-07-30 15:37:28 +0530

    terraform

---
+++ 12-provisioners/ec2.tf
@@ -0,0 +1,72 @@
+resource "aws_instance" "instance" {
+  for_each = var.components
+  ami           = data.aws_ami.ami.image_id
+  vpc_security_group_ids = data.aws_security_groups.sg.ids
+  instance_type = each.value["instance_type"] # as instance type is a map inside component variable
+
+  tags = {
+    Name = "${each.key}dev"
+  }
+  provisioner "remote-exec" {
+    connection {
+      user  = "ec2-user"
+      password = "Devops321"
+      host  = self.private_ip
+    }
+    #from here execution happens with inline
+    inline = [
+      "sudo pip-3.11 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/prashanthkp3103/roboshop-ansible/main.yml -e env=dev -e role_name=${each.key}"
+
+    ]
+  }
+}
+
+
+resource "aws_route53_record" "frontend" {
+  for_each = var.components
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "${each.key}dev.${var.domain_name}"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.instance[each.key].private_ip]
+}
+
+
+# resource "aws_instance" "mongo" {
+#   count = length(var.components)
+#   ami           = data.aws_ami.ami.image_id
+#   vpc_security_group_ids = data.aws_security_groups.sg.ids
+#   instance_type = var.instance_type
+#
+#   tags = {
+#     Name = "mongo.dev"
+#   }
+# }
+#
+# resource "aws_route53_record" "mongo" {
+#   zone_id = data.aws_route53_zone.zone.zone_id
+#   name    = "mongo.dev.${var.domain_name}"
+#   type    = "A"
+#   ttl     = 15
+#   records = [aws_instance.mongo.private_ip]
+# }
+#
+#
+# resource "aws_instance" "catalogue" {
+#   ami           = data.aws_ami.ami.image_id
+#   vpc_security_group_ids = data.aws_security_groups.sg.ids
+#   instance_type = var.instance_type
+#
+#   tags = {
+#     Name = "catalogue.dev"
+#   }
+# }
+#
+# resource "aws_route53_record" "catalogue" {
+#   zone_id = data.aws_route53_zone.zone.zone_id
+#   name    = "catalogue.dev.${var.domain_name}"
+#   type    = "A"
+#   ttl     = 15
+#   records = [aws_instance.catalogue.private_ip]
+# }
---
+++ 12-provisioners/vars.tf
@@ -0,0 +1,34 @@
+variable "ami" {
+  default = "ami-041e2ea9402c46c32"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+  default = "sg-03ec8b1ac2604e9da"
+}
+
+variable "zoneid" {
+  default = "Z0668859214N41P8Y7GLH"
+}
+
+variable "domain_name" {
+  default = "meppk.xyz"
+}
+
+
+variable "components" {
+  default = {
+    frontend = {
+      instance_type = "t3.micro"
+    }
+    mongo = {
+      instance_type = "t3.small"
+    }
+    catalogue = {
+      instance_type = "t3.micro"
+    }
+  }
+}
