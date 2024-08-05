443afb836307d955bb7e205373c03c7e985273fc
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-08-05 13:29:31 +0530

    terraform

--- roboshop-v5/ec2.tf
+++ roboshop-v5/ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "instance" {
   instance_type = each.value["instance_type"] # as instance type is a map inside component variable
 
   tags = {
-    Name = "${each.key}dev"
+    Name = "${each.key}.dev"
   }
 
 }
@@ -14,7 +14,7 @@ resource "aws_instance" "instance" {
 resource "aws_route53_record" "dns_record" {
   for_each = var.components
   zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "${each.key}dev.${var.domain_name}"
+  name    = "${each.key}.dev.${var.domain_name}"
   type    = "A"
   ttl     = 15
   records = [aws_instance.instance[each.key].private_ip]
