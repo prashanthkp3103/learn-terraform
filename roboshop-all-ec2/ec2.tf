4013dc93d6a20ce6c02df5aa29b71c0c662cdcc4
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-08-21 07:07:31 +0530

    vault updated for ansible

--- roboshop-all-ec2/ec2.tf
+++ roboshop-all-ec2/ec2.tf
@@ -22,26 +22,26 @@ resource "aws_route53_record" "dns_record" {
 }
 #resource creation is completely isolated and provisioner is isolated ,it will execute only after above as we have depends_on
 
-resource "null_resource" "ansible" {
-  depends_on = [aws_route53_record.dns_record]
-  for_each = var.components
-
-  provisioner "remote-exec" {
-    connection {
-      user  = "ec2-user"
-      password = "DevOps321"
-      host  = aws_instance.instance[each.key].private_ip
-    }
-
-
-    #from here execution happens with inline
-    inline = [
-      "sudo pip-3.11 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/prashanthkp3103/Latest-Ansible-Roboshop main.yml -e env=dev -e role_name=${each.key}"
-
-    ]
-  }
-}
+# resource "null_resource" "ansible" {
+#   depends_on = [aws_route53_record.dns_record]
+#   for_each = var.components
+#
+#   provisioner "remote-exec" {
+#     connection {
+#       user  = "ec2-user"
+#       password = "DevOps321"
+#       host  = aws_instance.instance[each.key].private_ip
+#     }
+#
+#
+#     #from here execution happens with inline
+#     inline = [
+#       "sudo pip-3.11 install ansible",
+#       "ansible-pull -i localhost, -U https://github.com/prashanthkp3103/Latest-Ansible-Roboshop main.yml -e env=dev -e role_name=${each.key}"
+#
+#     ]
+#   }
+# }
 
 
 # resource "aws_instance" "mongo" {
